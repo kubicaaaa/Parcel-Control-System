74de424972aae997b163375d94ee30a96aab943a
Alex <chabrowski31@gmail.com>
2026-05-25 18:07:20 +0200

    Structural changes

--- app/src/main/java/app/repository/ParcelRepository.java
+++
@@ -1,7 +0,0 @@
-package app.repository;
-
-import app.model.Parcel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParcelRepository extends JpaRepository<Parcel, Integer> {
-}
